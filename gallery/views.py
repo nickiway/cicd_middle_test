a2303afa6d25871bb61b418255269c174608a528
LinaSyrota <angelinasirotaalex@gmail.com>
2023-05-23 13:18:06 +0300

    views

--- gallery/views.py
+++ gallery/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import View
 from .models import Image
 
+
 class gallery_view(View):
     def get(self, request):
         categories = Image.objects.values('category').distinct()
